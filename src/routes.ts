ca72d6eb659b49b19231ac6363bb6c8b2822c3ce
Satya Pramod <satyapramod5551@gmail.com>
2023-03-21 17:49:21 -0500

    feat: implemented for auth feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
+import { authRoutes } from '@auth/routes/authRoutes';
+import { currentUserRoutes } from '@auth/routes/currentRoutes';
+import { serverAdapter } from '@service/queues/base.queue';
 import { Application } from 'express';
+import { authMiddleware } from '@global/handlers/auth-middleware';
 
+const BASE_PATH = '/api/v1'
 
 export default(app: Application) => {
     const routes = () => {
+        app.use('/queues', serverAdapter.getRouter());
+        app.use(BASE_PATH, authRoutes.routes());
+        app.use(BASE_PATH, authRoutes.signoutRoute());
 
+        app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
     }
     routes();
 }
